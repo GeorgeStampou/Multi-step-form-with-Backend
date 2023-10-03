b1c2b80d22776a7406f2ee8af81af1effe313e71
George Stampoulidis <georgios.stampoulidis@gmail.com>
2023-10-03 13:43:44 +0300

    init app js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Multi step form with backend");
+require("dotenv");
+const express = require("express");
+const app = express();
+
+app.use(express.static("./public"));
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port);
