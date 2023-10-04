f2ddcbc635defa6c6627e21c5e9a2632fc3baf4e
George Stampoulidis <georgios.stampoulidis@gmail.com>
2023-10-04 19:46:28 +0300

    bcrypt added for code encyption

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const Users = require("../models/users");
 const {createCustomError} = require("../errors/custom-error");
+const bcrypt = require("bcrypt");
 
 const getAllUsers = async (req, res) => {
     try {
@@ -12,10 +13,11 @@ const getAllUsers = async (req, res) => {
 
 const createUser = async (req, res) =>{
     try {
+        const salt = await bcrypt.genSalt();
+        req.body.password = await bcrypt.hash(req.body.password, salt);
         const user = await Users.create(req.body);
         res.status(201).json({user});
     } catch (error) {
-        //console.log(error.message);
         res.status(500).json({msg: error.message});
     }
 }
